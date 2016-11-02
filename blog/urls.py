14425926657b93a8d510d0a682d05b61b3376c47
jinsollee92 <jinsol.lee.92@gmail.com>
2016-11-01 22:14:53 -0400

    Added blog posts view

--- blog/urls.py
+++ blog/urls.py
@@ -3,4 +3,6 @@ from . import views
 
 urlpatterns = [
 	url(r'^$', views.index),
+	url(r'^blog/$', views.blog),
+	url(r'^blog/?page=(?P<num>[0-9]+)/$', views.blog),
 ]
--- blog/views.py
+++ blog/views.py
@@ -1,4 +1,29 @@
 from django.shortcuts import render
+from django.core.paginator import Paginator, EmptyPage, PageNotAnInteger
+
+from .models import *
+
 
 def index(request):
 	return render(request, 'blog/index.html')
+
+
+def blog(request, page=1):
+	all_posts = Post.objects.filter(published=True).order_by('-created_time')
+	post_paginator = Paginator(all_posts, 5)
+
+	page_range_start = max(1, page - 2)
+	page_range_end = min(post_paginator.num_pages + 1, page + 3)
+	page_range = range(page_range_start, page_range_end)
+
+	try:
+		post_list = post_paginator.page(page)
+	except EmptyPage:
+		post_list = post_paginator.page(1)
+
+	context = {
+		'post_list': post_list,
+		'page_range': page_range
+	}
+
+	return render(request, 'blog/posts.html', context)
